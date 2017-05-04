3927eb0521e3d15c34b49aa8a00b80c046a53f37
Eric Masiello <eric.j.masiello@gmail.com>
2017-05-03 20:39:32 -0400

    Add helmet middleware

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
 import uiRoutes from './routes/ui';
 import apiRoutes from './routes/api';
 
 const app = express();
+
+app.use(helmet());
 
 app.use('/api/v1', apiRoutes);
 app.use('/', uiRoutes);
