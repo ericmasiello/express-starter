e3e3935a70459ad11328a690b2acf9f541dd5c9b
Eric Masiello <eric.j.masiello@gmail.com>
2017-05-06 14:21:17 -0400

    add flow

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,4 @@
+/* @flow */
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
--- src/server/controllers/uiController.js
+++ src/server/controllers/uiController.js
@@ -1,11 +1,12 @@
+/* @flow */
 import logger from '../logger';
 
-export function main(request, response) {
+export function main(request: express$Request, response: express$Response): void {
   logger.info('Everything seems okay!');
   response.send('Okay!');
 }
 
-export function health(request, response) {
+export function health(request: express$Request, response: express$Response): void {
   response.json({
     status: 'ok',
   });
