cee58207a98f3e5deb03d864fe9cbc1c4c2beb5b
Eric Masiello <eric.j.masiello@gmail.com>
2017-05-06 20:13:49 -0400

    Add eslint to lint build

--- tools/webpack/webpack.server.babel.js
+++ tools/webpack/webpack.server.babel.js
@@ -17,6 +17,14 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        },
+      },
+      {
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
