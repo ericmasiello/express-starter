408b4af267d8c00333c4e447f8e375e6696444ad
Eric Masiello <eric.j.masiello@gmail.com>
2017-06-19 08:40:35 -0400

    Convert Detail to class based component in order to support HMR

--- src/client/Detail.js
+++ src/client/Detail.js
@@ -1,10 +1,15 @@
 /* @flow */
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Detail() {
-  return (
-    <div>
-      <h2>Detail page!</h2>
-    </div>
-  );
+// Note: Top level components must be class based for HMR to function
+/* eslint-disable react/prefer-stateless-function */
+export default class Detail extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Detail page!</h2>
+      </div>
+    );
+  }
 }
+/* eslint-enable react/prefer-stateless-function */
