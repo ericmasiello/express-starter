b7f9e997967b9046cc03d178be29d82e2b0d0e27
Eric Masiello <eric.j.masiello@gmail.com>
2017-06-24 13:48:46 -0400

    Remove unnecessary ejs dependency now that im not bundling server with webpack

--- src/server/app.js
+++ src/server/app.js
@@ -2,8 +2,6 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import { __express as ejs } from 'ejs';
-// import isdev from 'isdev';
 import compression from 'compression';
 import hotMiddleware from './middleware/hot';
 import uiRoutes from './routes/ui';
@@ -17,10 +15,6 @@ app.use(morgan(MORGAN_CONFIG));
 app.use(compression());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-// Note: this line is required for templates to work with webpack
-// http://stackoverflow.com/questions/41707662/webpack-express-ejs-error-cannot-find-module
-app.engine('.ejs', ejs);
 
 if (!isprod) {
   app.use(hotMiddleware);
