62ae1bd20e0a7cf055239e7b3cfb75920e6dd3bb
Eric Masiello <eric.j.masiello@gmail.com>
2017-06-29 17:25:50 -0400

    Add nodemon to monitor server changes in dev mode using babel-register hook. Adjust eslint rules to allow for import/no-extraneious-dependencies in certain files that are intended for development work

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,16 @@ module.exports = {
     'eslint-config-ericmasiello/jest',
     'eslint-config-ericmasiello/react',
   ],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: [
+        './postcss.config.js',
+        './webpack.config.js',
+        '**/*.test.js',
+        'src/www/dev.js',
+        'src/server/middleware/hot.js',
+        'src/server/util/routeMatch.js',
+      ],
+    }],
+  },
 };
