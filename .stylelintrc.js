9658654caf26e8b338c9732138c7030b12b34c2b
Eric Masiello <eric.j.masiello@gmail.com>
2017-08-08 13:55:23 -0400

    Add support for css modules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: 'stylelint-config-ericmasiello',
+  rules: {
+    'selector-pseudo-class-no-unknown': [ true, {
+      ignorePseudoClasses: [
+        'export',
+        'import',
+        'global',
+        'local',
+      ],
+    }],
+    'property-no-unknown': [ true, {
+      ignoreProperties: [
+        'composes',
+        'compose-with',
+      ],
+    }],
+    'at-rule-no-unknown': [ true, {
+      ignoreAtRules: [
+        'value',
+      ],
+    }],
+  },
 };
